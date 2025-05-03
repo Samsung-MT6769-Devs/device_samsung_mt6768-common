4e6f20722b45528025faba8812ae6129cfc04826
Zears14 <79321017+Zears14@users.noreply.github.com>
2025-05-03 14:38:16 +0700

    chore: remove compatibility matrix and manifest files
    Signed-off-by: Zears14 <79321017+Zears14@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,9 +61,6 @@ BOARD_MKBOOTIMG_ARGS := \
 	--dtb $(TARGET_PREBUILT_DTB) \
 	--dtb_offset $(BOARD_DTB_OFFSET)
 
-# Manifest
-DEVICE_MANIFEST_FILE ?= $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE ?= $(COMMON_PATH)/compatibility_matrix.xml
 
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 8539602944
